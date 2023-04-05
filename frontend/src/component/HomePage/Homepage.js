dbea8daa1bf0c69200fe800d5a40c6953aa87537
Krittika Tiwari <krittikatiwari122004@gmail.com>
2023-04-05 20:30:17 +0530

    created schema,fetch data from backend

--- frontend/src/component/HomePage/Homepage.js
+++ frontend/src/component/HomePage/Homepage.js
@@ -1,33 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import "./Homepage.css"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "./Homepage.css";
 
 function Homepage() {
+  const [category, setCategory] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:5000/category")
+      .then((res) => res.json())
+      .then((data) => {
+        setCategory(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(category);
   return (
-    <div className='Homepage'>
-        <div className='image-courosel'>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={1}>
-          <SwiperSlide><div className="image">
-            <img src="https://i0.wp.com/swadishta.de/wp-content/uploads/2021/01/Idle.jpg?fit=1200%2C800&ssl=1" alt="img"/></div></SwiperSlide>
-          <SwiperSlide><div className="image">
-            <img src="https://cdn.sightseeingtoursitaly.com/wp-content/uploads/2019/06/Famous-Italian-dishes.jpg" alt="img"/></div></SwiperSlide>
-          <SwiperSlide><div className="image">
-            <img src="https://www.fryingpanadventures.com/wp-content/uploads/2020/10/image-137-scaled-e1604152201367.jpeg" alt="img"/></div></SwiperSlide>
-          <SwiperSlide><div className="image">
-            <img src="https://visualeducation.com/wp-content/uploads/2019/08/Home-cooking-2148.jpg" alt="img"/></div></SwiperSlide>
+    <div className="Homepage">
+      <div className="image-courosel">
+        <Swiper spaceBetween={50} slidesPerView={1}>
+          <SwiperSlide>
+            <div className="image">
+              <img
+                src="https://i0.wp.com/swadishta.de/wp-content/uploads/2021/01/Idle.jpg?fit=1200%2C800&ssl=1"
+                alt="img"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="image">
+              <img
+                src="https://cdn.sightseeingtoursitaly.com/wp-content/uploads/2019/06/Famous-Italian-dishes.jpg"
+                alt="img"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="image">
+              <img
+                src="https://www.fryingpanadventures.com/wp-content/uploads/2020/10/image-137-scaled-e1604152201367.jpeg"
+                alt="img"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="image">
+              <img
+                src="https://visualeducation.com/wp-content/uploads/2019/08/Home-cooking-2148.jpg"
+                alt="img"
+              />
+            </div>
+          </SwiperSlide>
           ...
         </Swiper>
+      </div>
+      <div className="country">
+        {/* <h1 className='cuisine'>Cuisine</h1> */}
+        <div className="cuisine-container">
+          {category.map((e, index) => {
+            return (
+              <div key={index} className="cuisine-box">
+                <img
+                  src={e.countryImage}
+                  alt="img"
+                />
+                <h2>{e.country} Food</h2>
+              </div>
+            );
+          })}
         </div>
-        <div className='country'>
-            <h1 className='cousine'>Cousine</h1>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
--- frontend/src/component/Navbar/Navbar.js
+++ frontend/src/component/Navbar/Navbar.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./Navbar.css";
 import Signup from "./signup";
 import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar-menu">
-        <div className="logo">CraveSavor</div>
+        <Link className="abc" to='/'><div className="logo">CraveSavor</div></Link>
         <div className="menubar-wrap">
-          <div className="menu">Home</div>
-          <div className="menu">Save</div>
-          <div className="menu">About Us</div>
+        <Link className="abc" to='/'><div className="menu">Home</div></Link>
+        <Link className="abc" to='/save'><div className="menu">Save</div></Link>
+        <Link className="abc" to='/about-us'><div className="menu">About Us</div></Link>
         </div>
         <Signup />
       </div>
