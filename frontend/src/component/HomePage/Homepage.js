a5a194639c8a821833df14cc90e7b9a959e90b69
Krittika Tiwari <krittikatiwari122004@gmail.com>
2023-04-28 12:09:58 +0530

    search bar function

--- frontend/src/component/HomePage/Homepage.js
+++ frontend/src/component/HomePage/Homepage.js
@@ -120,18 +120,14 @@ function Homepage() {
           <h1>Welcome to CraveSavor</h1>
           <h2>DISCOVER OUR STORY</h2>
           <div>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
+            when you want to make someting you go to youtube and search the
+            recipe but you end up finding so many video of same recipe and you
+            don't know which one is good, to find best video you watch so many
+            video which take lot of time. To sole this problem I am making this
+            website where you can find best video on youtube and there recipe.
           </div>
           <div>
-            It was popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
+            
           </div>
           <Link to="/about-us">
             <button className="buttn">Read More</button>
--- frontend/src/component/Navbar/Searchbar.js
+++ frontend/src/component/Navbar/Searchbar.js
@@ -1,19 +1,86 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { useState, useEffect } from "react";
+import axios from "axios"
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Searchbar() {
-  const [searchItem,setSearchItem]=useState([])
-  
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  useEffect(() => {
+    const searchList = async () => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API}/recipe`
+      );
+      setData(res.data);
+    };
+    searchList();
+  }, []);
+
+
+  const reverseSearch = () => {
+    const searchForm = document.querySelector(".input-wrapper");
+    searchForm.classList.toggle("active");
+    setValue("");
+  };
+
+
+   
   return (
     <div className="menu-bar">
       <div className="input-wrapper">
         <FaSearch id="search-icon" />
-        <input placeholder="Type to search ..." />
+        <input
+          placeholder="Type to search ..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
       </div>
-    </div>
+      <div className="dropDown" onClick={reverseSearch}>
+                {data
+                  .filter((e) => {
+                    const searchedTerm = value.toLocaleLowerCase().trim();
+                    let RecipeName = e.name;
+                    const recipeName = RecipeName.toLocaleLowerCase().trim();
+
+                    return (
+                      (searchedTerm && recipeName.startsWith(searchedTerm)) ||
+                      (recipeName === searchedTerm && recipeName !== searchedTerm)
+                    );
+                  })
+                  .slice(0, 5)
+                  .map((i, index) => {
+                    return (
+                      <>
+                        <Link className="abc" to={`/indian/${i._id}`} key={index}>
+                          <div
+                            className="search-recipe-div"
+                            onClick={() => setValue(i.name)}
+                          >
+                            <div>
+                              <h2>
+                                {i.name}
+                              </h2>
+                      
+                              <span style={{ color: "var(--marigold)" }}>
+                                {i.currentMin}
+                              </span>
+                            </div>
+                          </div>
+                        </Link>
+                      </>
+                    );
+                  })}
+              </div>
+            </div>
   );
 }
 
 export default Searchbar;
+
